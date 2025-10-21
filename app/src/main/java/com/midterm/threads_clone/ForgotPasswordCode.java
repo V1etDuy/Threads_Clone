2c18b7f10608b1ab3781f65e32a6b8abc39b1b9b
V1etDuy <tdv0905179758@gmail.com>
2025-10-21 11:56:10 +0700

    feat(auth): add forgotPassword screen UI

---
+++ app/src/main/java/com/midterm/threads_clone/ForgotPasswordCode.java
@@ -0,0 +1,14 @@
+package com.midterm.threads_clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_code);
+    }
+}
